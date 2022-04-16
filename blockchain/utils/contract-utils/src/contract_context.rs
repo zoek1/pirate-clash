d1411c250d078c29658a1d2020fee893f273e1d7
Miguel Gordian <os.aioria@gmail.com>
2022-04-16 03:05:43 -0500

    Add updated utils for blockchains contracts

---
+++ blockchain/utils/contract-utils/src/contract_context.rs
@@ -0,0 +1,36 @@
+use casper_contract::unwrap_or_revert::UnwrapOrRevert;
+use casper_types::{system::CallStackElement, Key};
+
+use crate::ContractStorage;
+
+pub trait ContractContext<Storage: ContractStorage> {
+    fn storage (&self) -> Storage;
+
+    fn get_caller(&self) -> Key {
+        let storage = self.storage();
+        let call_stack = storage.call_stack();
+        let caller = call_stack.get(call_stack.len() - 2);
+        element_to_key(caller.unwrap_or_revert())
+    }
+
+    fn self_addr(&self) -> Key {
+        let storage = self.storage();
+        let call_stack = storage.call_stack();
+        element_to_key(call_stack.last().unwrap_or_revert())
+    }
+}
+
+fn element_to_key(element: &CallStackElement) -> Key {
+    match *element {
+        CallStackElement::Session { account_hash } => (account_hash).into(),
+        CallStackElement::StoredSession {
+            account_hash,
+            contract_package_hash: _,
+            contract_hash: _,
+        } => (account_hash).into(),
+        CallStackElement::StoredContract {
+            contract_package_hash,
+            contract_hash: _,
+        } => (contract_package_hash).into(),
+    }
+}
